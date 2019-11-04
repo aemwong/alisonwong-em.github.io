e172358136e9fddd837651580e7a31d18f1906b8
Alison <alisonnnw@gmail.com>
2019-11-04 09:53:16 -0500

    updated resume file and linking button

---
+++ src/assets/sass/colors.js
@@ -0,0 +1,15 @@
+const turquoise = "#0091B4";
+const darkGray = "#3B3B3B";
+const lightPurple = "#C6CAED";
+const pink = "#F7F0F5";
+const white = "#ffffff";
+
+const colors = {
+    turquoise,
+    darkGray,
+    lightPurple,
+    pink,
+    white,
+};
+
+export default colors;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,25 +3,39 @@ import Box from '@material-ui/core/Box';
 import './Landing.scss';
 import helloGif from '../../assets/img/helloAnimation.gif'
 import introPic from '../../assets/img/landingImg.jpg'
-
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import colors from "../../assets/sass/colors";
+import resume from "../../assets/img/AlisonWong_Resume.pdf";
 
 class Landing extends Component {
-    render() {
-        return (
-            <Box className="landingContainer" display="flex" flexDirection="row-reverse" flexWrap="wrap"
-                justifyContent="space-evenly" alignItems="center">
-                <Box m={"2vw"} ml={0} className="halfContainer" display="flex" justifyContent="center">
-                    <img src={introPic} id="introPic"></img>
-                </Box>
-                <Box m={"2vw"} display="flex" flexDirection="column" alignItems="center" className="halfContainer">
 
-                    <img src={helloGif} id="helloAnimation"></img>
-                    <div className="leftAlign title">I'm Alison <span role="img" aria-label="smiley-face">🤗</span></div>
-                    <Box ml={"5vw"} ><div className="leftAlign">an aspiring product manager continually growing and welcoming new opportunities</div>
-                        <br />
-                        <div className="italic leftAlign">Currently in search of a Summer 2020 Product Internship</div></Box>
-                </Box></Box>
+    render() {
+        const theme = createMuiTheme({
+            palette: {
+                primary: { main: colors.turquoise }
+            }
+        }
         )
+
+        return (
+            <ThemeProvider theme={theme}>
+                <Box className="landingContainer" display="flex" flexDirection="row-reverse" flexWrap="wrap"
+                    justifyContent="space-evenly" alignItems="center">
+                    <Box m={"2vw"} ml={0} className="halfContainer" display="flex" justifyContent="center">
+                        <img src={introPic} id="introPic"></img>
+                    </Box>
+                    <Box m={"2vw"} display="flex" flexDirection="column" alignItems="center" className="halfContainer">
+
+                        <img src={helloGif} id="helloAnimation"></img>
+                        <div className="leftAlign title">I'm Alison <span role="img" aria-label="smiley-face">🤗</span></div>
+                        <Box ml={"5vw"} ><div className="leftAlign">an aspiring product manager continually growing and welcoming new opportunities</div>
+                            <br />
+                            <div className="italic leftAlign">Currently in search of a Summer 2020 Product Internship</div></Box>
+                        <br/>
+                        <Button variant="outlined" color="primary" id="resumeButton" target="_blank" href={resume}>View Resume</Button>
+                    </Box></Box>
+            </ThemeProvider>)
     }
 }
 
