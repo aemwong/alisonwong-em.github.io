bb84044e72ba8f0e9458eaf2be50c9927041b7e4
Alison <alisonnnw@gmail.com>
2019-11-16 14:58:04 -0500

    Finished projects mapping

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,41 +3,51 @@ import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import colors from "../../assets/sass/colors";
 import StrumMaster from "../../assets/img/StrumMasterPreview.jpg";
+import Website from "../../assets/img/WebsiteMockupPreview.jpg";
+import Coorporation from "../../assets/img/CoorpPreview.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 import './Projects.scss';
 
 
 class Projects extends Component {
     render() {
+
+        const projectTitles = [
+            {
+                img: StrumMaster,
+                title: 'Strum Master',
+                subtitle: 'designing a modified guitar for recovering stroke patients'
+            }, {
+                img: Website,
+                title: 'Alisonwong-em',
+                subtitle: 'a personal website to showcase 3projects'
+            }, {
+                img: Coorporation,
+                title: 'Co-orporation!',
+                subtitle: 'a game advocating for government change'
+            },
+
+        ]
         return (
             <Box className="projectsContainer" display="flex" flexDirection="column" flexWrap="wrap" justifyItems="space-evenly">
                 <div className="italic" style={{ margin: '20px 0' }}>What I've been up to recently...</div>
-                <Paper style={{ borderRadius: '7px 0 0 7px', marginLeft: 'calc(5vw + 20px)', marginTop: '20px' }}>
-                    <Box m={3} className="leftAlign" flexDirection="row">
-                        <div style={{ color: colors.turquoise }}>
-                            hi this is the first project</div>
-                        <div>stufff </div>
-                    </Box>
-                </Paper>
+                {projectTitles.map((project) =>
 
-                <Paper style={{ borderRadius: '7px 0 0 7px' }} className="project">
-                    <Box m={2} className="leftAlign" flexDirection="row" justifyContent="flex-start" display="flex" alignItems="center">
-                        <img src={StrumMaster} className="projectImg" />
-                        <Box flexDirection="column" display="flex" justifyContent="center" m={2}>
-                            <div style={{ color: colors.turquoise }}>Strum Master</div>
-                            <div style={{ fontSize: '12pt' }}>designing a modified guitar for recovering stroke patients </div>
+                    <Paper style={{ borderRadius: '7px 0 0 7px', paddingRight: '20px' }} className="project">
+                        <Box m={2} className="leftAlign" flexDirection="row" justifyContent="flex-start" display="flex" alignItems="center">
+                            <img src={project.img} className="projectImg" />
+                            <Box flexDirection="column" display="flex" justifyContent="center" m={2}>
+                                <div style={{ color: colors.turquoise }}>{project.title}</div>
+                                <div style={{ fontSize: '12pt' }}>{project.subtitle}</div>
+                            </Box>
+                            <Box className="rightAlign" flexGrow={1}>
+                                <FontAwesomeIcon icon={faArrowCircleRight} color={colors.turquoise} />
+                            </Box>
                         </Box>
+                    </Paper>)
 
-                    </Box>
-                </Paper>
-                <Paper style={{ borderRadius: '10px 0 0 10px', marginLeft: 'calc(5vw + 20px)', marginTop: '20px' }}>
-                    <Box m={3} className="leftAlign">
-                        <div style={{ color: colors.turquoise }}>
-                            hi this is the first project</div>
-                        <div>stufff </div>
-                    </Box>
-                </Paper>
-
-
+                }
             </Box>
         )
     }
