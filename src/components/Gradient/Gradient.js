fd43e4c508e41e885fb83291f8987e098131fee6
Alison <alisonnnw@gmail.com>
2020-01-10 00:53:48 -0500

    removed scroll-x

--- src/components/Gradient/Gradient.js
+++ src/components/Gradient/Gradient.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Gradient extends Component {
     render() {
         return (
-            <div style={{ backgroundImage: `linear-gradient(${this.props.color1}, ${this.props.color2})`, height: this.props.height }}></div >
+            <div style={{ backgroundImage: `linear-gradient(${this.props.color1}, ${this.props.color2})`, height: this.props.height, width: window.innerWidth }}></div >
         )
     }
 }
+
 
 export default Gradient;
