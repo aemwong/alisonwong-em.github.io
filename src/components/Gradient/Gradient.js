2e4e5b84ebeb3b6eb9c7a5898fbe9757d79bfcbe
Alison <alisonnnw@gmail.com>
2020-01-13 15:12:18 -0500

    fixed gradient innerwidth

--- src/components/Gradient/Gradient.js
+++ src/components/Gradient/Gradient.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Gradient extends Component {
     render() {
         return (
-            <div style={{ backgroundImage: `linear-gradient(${this.props.color1}, ${this.props.color2})`, height: this.props.height, width: window.innerWidth }}></div >
+            <div style={{ backgroundImage: `linear-gradient(${this.props.color1}, ${this.props.color2})`, height: this.props.height, width: '100vw' }}></div >
         )
     }
 }
